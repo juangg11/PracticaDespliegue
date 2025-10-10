419384acf976f0bda343ecc7ac2912587db133a5
Ubuntu <ubuntu@ip-172-31-22-168.ec2.internal>
2025-10-10 12:21:52 +0000

    Commit2

--- scripts/install_lamb.sh
+++ scripts/install_lamb.sh
@@ -6,4 +6,12 @@ apt update
 
 apt upgrade
 
-sudo apt install apache2 -y
+apt install apache2 -y
+
+apt install php libapache2-mod-php php-mysql -y
+
+cp ../conf/000-default.conf /etc/apache2/sites-available/000-default.conf
+
+systemctl restart apache2
+
+cp ../php/index.php /var/www/html
